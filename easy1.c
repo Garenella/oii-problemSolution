622c100854cec109c018ff22d7422481400b79bc
Gimmy89 <Gimmy89@users.noreply.github.com>
2017-07-11 18:09:52 +0200

    solution to easy1 problem

---
+++ easy1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(int argc, char const *argv[])
+{
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);	
+
+	int N, max = INT_MIN, tmp;
+	printf("%d\n", tmp);
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		scanf("%d", &tmp);
+		if (max < tmp)
+		{
+			max = tmp;
+		}
+	}
+
+	printf("%d\n", max);
+
+	return 0;
+
+}
