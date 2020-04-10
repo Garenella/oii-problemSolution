5dc9fa5864ad09a474d142b29d3d38536da65a18
Garenella <Gimmy89@users.noreply.github.com>
2020-04-10 19:07:59 +0200

    solution improvement to "trasposta" problem

--- trasposta.c
+++ trasposta.c
@@ -2,65 +2,32 @@
 #include <limits.h>
 #include <stdlib.h>
 
-void alloca_matrice(int r, int c) ;
-void dealloca_matrice(int r) ;
-void carica_matrice(int r, int c) ;
-void stampa_matrice(int r, int c) ;
-
-int n, m;
-long **a;
 
 int main(int argc, char const *argv[])
 {
+    int n, m, size;
+    long *a;
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);	
 	scanf("%d %d", &n, &m);
-    alloca_matrice(n, m);
-    carica_matrice(n, m);
-	stampa_matrice(n, m);
-    dealloca_matrice(n);
-	return 0;
-}
-
-void alloca_matrice(int r, int c) 
-{
-    a = (long **)malloc(r*sizeof(long *));
-    for (int i=0; i<r; i++)
-        a[i] = (long *) malloc(c*sizeof(long));
-    return;
-}
-
-void dealloca_matrice(int r) 
-{
-    for (int i = 0; i < r; i++){  
-       free(a[i]);  
-    }     
-    free(a);
-    return;
-}
-
-void carica_matrice(int r, int c) 
-{
-    for (int i = 0; i < r; i++)
+    size = n * m;
+    //alloca matrice
+    a = (long *)malloc((n*m)*sizeof(long ));
+    //carica valori
+    for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < c; j++)
-        {
-           scanf("%ld", &a[i][j]);
-        }
-        
+        scanf("%ld", &a[i]);
     }
-    return;
-}
-
-void stampa_matrice(int r, int c) 
-{
-    for (int j = 0; j < c; j++)
+    //stampa traposta
+    for (int j = 0; j < m; j++)
     {
-        for (int i = 0; i < r; i++)
+        for (int i = j; i < size; i+=m)
         {
-           printf("%ld ", a[i][j]);
+           printf("%ld ", a[i]);
         }
         printf("\n");
     }
-    return;
+    //dealloca matrice
+    free(a);
+    return 0;
 }
